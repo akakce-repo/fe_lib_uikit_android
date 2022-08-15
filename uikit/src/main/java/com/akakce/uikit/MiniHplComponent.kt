ff0ca21833ba4860223e669e9fa6aa9aa39b587e
selimsimsek <selim@akakce.com>
2022-08-15 18:30:11 +0300

    Mini hpl componenti eklendi.

---
+++ uikit/src/main/java/com/akakce/uikit/MiniHplComponent.kt
@@ -0,0 +1,37 @@
+package com.akakce.uikit
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.akakce.uikit.databinding.MiniHplComponentBinding
+
+/**
+ * @Author: Selim Simsek
+ * @Date: 15.08.2022
+ */
+class MiniHplComponent : ConstraintLayout {
+    lateinit var binding: MiniHplComponentBinding
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    fun init() {
+        val inflater = (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+        binding = MiniHplComponentBinding.inflate(inflater, this, true)
+    }
+
+}
